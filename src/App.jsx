bf234ed3cdade2393179b5009c24fb1f5b1b1ad1
Krishna450 <narayanamkrishna450@gmail.com>
2024-09-11 17:23:05 +0530

    UncontrolledTask

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { CustomBulb } from "./Tasks/bulbTask"
-import FakeStoreCategories from "./Tasks/Categories/FakeStoreCategories"
-import { ControlledForm } from "./Tasks/SamsungForm/ControlledForm"
-import CustomTodos from "./Tasks/Todos/todos"
+
+
+
 import CustomForm from "./Tasks/UncontrolledForm/unControlledForm"
 
 
 function App() {
    return(
-    // <FakeStoreCategories/>
-    <ControlledForm/>
+    <CustomForm/>
+    
    )
 }
 export default App
